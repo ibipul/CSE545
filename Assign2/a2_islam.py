64bafc901c3d4ace7c6d6c18c2eaf6f0b5a9cdad
ibipul <bipul.islam@stonybrook.edu>
2017-11-25 18:06:18 -0500

    Assignment 2 updates

--- Assign2/a2_islam.py
+++ Assign2/a2_islam.py
@@ -1,19 +1,19 @@
 from tifffile import TiffFile
 import io
+import time
 import zipfile
 import hashlib
 import numpy as np
 import scipy.stats as ss
 from numpy.linalg import svd
+from operator import add
+from pyspark import SparkContext
+sc = SparkContext('local/aws-cluster', 'Assignment 2')
 
+_LOCAL_DIR_PATH = "C:\\Users\\ibipul\\codes\\datasets\\a2_small_sample\\"
+_LOCAL_FILES_REGEX = _LOCAL_DIR_PATH + '*'
 _CHUNK_SIZE = 38
 
-def readFileInBinary(filename):
-    with open(filename, 'rb') as f:
-        fbin = f.read()
-    f.close()
-
-    return fbin
 
 def getOrthoTif(zfBytes):
     #given a zipfile as bytes (i.e. from reading from a binary file),
@@ -26,12 +26,14 @@ def getOrthoTif(zfBytes):
             tif = TiffFile(io.BytesIO(zfiles.open(fn).read()))
             return tif.asarray()
 
-def getTiffAsMatrix(file_path):
-    fbinary = readFileInBinary(file_path)
+def getTiffAsMatrix(kv):
+    file_name = kv[0].split('/')[-1]
+    fbinary = kv[1]
     tiffmat = getOrthoTif(fbinary)
-    return tiffmat
+    return (file_name,tiffmat)
 
-def splitTiffArray(tiffmat, filename='dummy.zip'):
+def splitTiffArray(kv):
+    filename, tiffmat= kv[0], kv[1]
     # Each image is 500x500
     kv_list = []
     if len(tiffmat) == 2500:
@@ -41,13 +43,11 @@ def splitTiffArray(tiffmat, filename='dummy.zip'):
         row_col_chunks = [(i, i + 500) for i in range(0, 5000, 500)]
     else:
         raise ValueError("TIFF file has dimensions other than 2500x2500 or 5000x5000")
-
     cell_counter = 0
     for x in row_col_chunks:
         for y in row_col_chunks:
             kv_list.append((filename+'-'+str(cell_counter),tiffmat[x[0]:x[1],y[0]:y[1]]))
             cell_counter +=1
-
     return kv_list
 
 def tilePixelIntensityConverter(kv):
@@ -124,4 +124,53 @@ def lshTransform(kv, band_count = 8):
         band_component = img_sig[i:(i+row_size)]
         band_hash_list.append(customHashFunc(band_component))
     # return something like ((file_name, band_id), bucket_number)
-    return [(band_hash_list[id],(id,img_name)) for id in range(len(band_hash_list))]
+    return [((band_hash_list[id],id),img_name) for id in range(len(band_hash_list))]
+
+def filter4Sample(kv):
+    val_list = kv[1]
+    if '3677454_2025195.zip-0' in val_list or \
+       '3677454_2025195.zip-1' in val_list or \
+       '3677454_2025195.zip-18' in val_list or \
+       '3677454_2025195.zip-19' in val_list:
+        return True
+    else:
+        return False
+
+def candidateExtraction(kv):
+    val_list = kv[1]
+    if '3677454_2025195.zip-0' in val_list:
+        val_list.remove('3677454_2025195.zip-0')
+        return ('3677454_2025195.zip-0', val_list)
+    elif '3677454_2025195.zip-1' in val_list:
+        val_list.remove('3677454_2025195.zip-1')
+        return ('3677454_2025195.zip-1', val_list)
+    elif '3677454_2025195.zip-18' in val_list:
+        val_list.remove('3677454_2025195.zip-18')
+        return ('3677454_2025195.zip-18', val_list)
+    elif '3677454_2025195.zip-19' in val_list:
+        val_list.remove('3677454_2025195.zip-19')
+        return ('3677454_2025195.zip-19', val_list)
+
+# Timing tracker
+start_time = time.time()
+rdd = sc.binaryFiles(_LOCAL_FILES_REGEX) #
+#rdd = sc.binaryFiles('hdfs:/data/small_sample')
+rdd2 = rdd.map(lambda kv: getTiffAsMatrix(kv))
+rdd3 = rdd2.map(lambda kv: splitTiffArray(kv)).flatMap(lambda xs : [x for x in xs])
+rdd4 = rdd3.map(lambda kv: tilePixelIntensityConverter(kv))
+rdd5 = rdd4.map(lambda kv: downScaleResolution(kv))
+rdd6 = rdd5.map(lambda kv: getRowColDiffFeatureVec(kv))
+rdd7 = rdd6.map(lambda kv: getImageSignature(kv))
+rdd8 = rdd7.map(lambda kv: lshTransform(kv,16)).flatMap(lambda xs : [x for x in xs])
+rdd9 = rdd8.groupByKey().mapValues(list)
+rdd10 = rdd9.filter(lambda kv: filter4Sample(kv))
+rdd11 = rdd10.map(lambda kv: candidateExtraction(kv)).filter(lambda kv: True if len(kv[1])>0 else False)
+z= rdd11.collect()
+end_time = time.time()
+print("Time elapsed: ", (end_time - start_time)/60,' mins \n')
+rdd12 = rdd11.reduceByKey(add).mapValues(list)
+z = rdd12.collect()
+end_time = time.time()
+print("Time elapsed: ", (end_time - start_time)/60,' mins \n')
+for i in z:
+    print('Candidates similar to : ', i[0],' are: ', set(i[1]))

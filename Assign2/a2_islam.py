88714f16e412708173a0e423c6a5b46af630bfb1
ibipul <bipul.islam@stonybrook.edu>
2017-11-26 22:59:37 -0500

    Presubmission State

--- Assign2/a2_islam.py
+++ Assign2/a2_islam.py
@@ -5,18 +5,20 @@ import zipfile
 import hashlib
 import numpy as np
 from pprint import pprint
-import scipy.stats as ss
 from scipy import linalg
-from operator import add
 from collections import defaultdict
 from pyspark import SparkContext
-sc = SparkContext('local', 'Assignment 2')
+sc = SparkContext(appName='Assignment_2: Bipul Islam')
 
 _LOCAL_DIR_PATH = "C:\\Users\\ibipul\\codes\\datasets\\a2_small_sample\\"
 _LOCAL_FILES_REGEX = _LOCAL_DIR_PATH + '*'
 _CHUNK_SIZE = 38
 
-
+###################
+##
+##  FUNCTION SECTION
+##
+####################
 def getOrthoTif(zfBytes):
     # This function has been provided
     #given a zipfile as bytes (i.e. from reading from a binary file),
@@ -41,30 +43,42 @@ def getTiffAsMatrix(kv):
     tiffmat = getOrthoTif(fbinary)
     return (file_name,tiffmat)
 
-def splitTiffArray(kv):
+def tiffmatrixSplit(kv):
     """
-    Given a (key, image arrary) splits it into
-    list of [(key, 500x500 nd array),(key, 500x500 nd array)...]
-    :param kv tuple:
+    Given a (k,V) tuple with key as file name , value as 2500x2500x4 matrix
+    We reduce it to component sub-images of size 500x500x4
+    On receiving [(img, 2500x2500x4)]
+    We return: [(img-0, 500x500x4),(img-1, 500x500x4)...(img-24, 500x500x4)] as a list.
+    :param kv tupple:
     :return:
-    :rtype list[]
+    :rtype list[]:
     """
-    filename, tiffmat= kv[0], kv[1]
+    filename, tiffmat = kv[0], kv[1]
     # Each image is 500x500
     kv_list = []
     if len(tiffmat) == 2500:
-        # 25 images case
-        row_col_chunks = [(i,i+500) for i in range(0,2500,500)]
+        num_matrices = 5**2
+        split_size = 5
     elif len(tiffmat) == 5000:
-        row_col_chunks = [(i, i + 500) for i in range(0, 5000, 500)]
+        num_matrices = 10**2
+        split_size = 10
     else:
         raise ValueError("TIFF file has dimensions other than 2500x2500 or 5000x5000")
-    cell_counter = 0
-    for x in row_col_chunks:
-        for y in row_col_chunks:
-            kv_list.append((filename+'-'+str(cell_counter),tiffmat[x[0]:x[1],y[0]:y[1]]))
-            cell_counter +=1
-    return kv_list
+    all_matrices = []
+    file_names = [filename + '-' + str(i) for i in np.arange(num_matrices)]
+    big_rows = np.vsplit(tiffmat, split_size)
+    for row in big_rows:
+        all_matrices += np.hsplit(row, 5)
+    return list(zip(file_names,all_matrices))
+
+def pixelConverter(pixels):
+    """
+    This is the pixel smoothing function. It's applied to each 1x4 pixel element.
+    :param pixels:
+    :return:
+    """
+    intensity = int((sum(pixels[:3]) / 3) * (pixels[-1] / 100))
+    return intensity
 
 def tilePixelIntensityConverter(kv):
     """
@@ -77,19 +91,14 @@ def tilePixelIntensityConverter(kv):
     """
     file_name = kv[0]
     tile = kv[1]
-    intensity_converted_img = []
-    for row in tile:
-        row_intensities = []
-        for pixel in row:
-            intensity = int((sum(pixel[:3])/3) * (pixel[-1]/100))
-            row_intensities.append(intensity)
-        intensity_converted_img.append(row_intensities)
-    return (file_name, np.array(intensity_converted_img))
+    intensity_converted_img = np.apply_along_axis(pixelConverter, 2, tile)
+    return (file_name, intensity_converted_img)
 
 def downScaleResolution(kv, factor=10):
     """
     Given a (filename, 500x500 2d array)
     returns (filename, 500x500 2d array downscaled by factor)
+
     :param kv:
     :param factor:
     :return:
@@ -97,19 +106,16 @@ def downScaleResolution(kv, factor=10):
     sub_img_name = kv[0]
     sub_image = kv[1]
     img_dimension = len(sub_image)
-    down_scaled_image = []
-    row_col_chunks = [(i,i+factor) for i in range(0,img_dimension,factor)]
-    for x in row_col_chunks:
-        row_factxfact_val = []
-        for y in row_col_chunks:
-            img_window = sub_image[x[0]:x[1],y[0]:y[1]]
-            row_factxfact_val.append(img_window.mean())
-        down_scaled_image.append(row_factxfact_val)
-    return (sub_img_name,np.array(down_scaled_image))
+    big_image = sub_image
+    Nbig = img_dimension
+    Nsmall = Nbig//factor
+    small_image = big_image.reshape([Nsmall, Nbig // Nsmall, Nsmall, Nbig // Nsmall]).mean(3).mean(1)
+    return (sub_img_name,small_image)
 
 def elementFilter(element):
     """
     Acts on each pixel of flattened arrays to threshold them to -1,0,1
+    This function is called in a vectorizes fashion inside getRowColDiffFeatureVec
     :param element int: intensity of a pixel
     :return:
     :rtype int:
@@ -133,11 +139,24 @@ def getRowColDiffFeatureVec(kv):
     img = kv[1]
     row_diff = np.diff(img)
     col_diff = np.diff(img.transpose())
-    row_diff_flattened = list(row_diff.flatten())
-    col_diff_flattened = list(col_diff.flatten())
-    feature_vector = row_diff_flattened + col_diff_flattened
-    features = [elementFilter(x) for x in feature_vector]
-    return (img_name, np.array(features))
+    feature_vector = np.concatenate((row_diff.flatten(),col_diff.flatten()))
+    vfunc = np.vectorize(elementFilter)
+    features = vfunc(feature_vector)
+    return (img_name, features)
+
+def applyMD5ToChunk(img_chunk,chunk_size):
+    """
+    This function applies md5 hash to a chunk of the feature vector.
+    It is called in a vectorized fashion inside getImageSignature
+    :param img_chunk:
+    :param chunk_size:
+    :return:
+    """
+    if len(img_chunk) < chunk_size:
+        return ''
+    else:
+        chunk_signature = hashlib.md5(img_chunk.tostring()).hexdigest()
+        return chunk_signature[int(len(chunk_signature)/2)]
 
 def getImageSignature(kv,chunk_size = _CHUNK_SIZE):
     """
@@ -150,18 +169,10 @@ def getImageSignature(kv,chunk_size = _CHUNK_SIZE):
     """
     img_name = kv[0]
     img = kv[1]
-    img_signature = ''
-    counter = 0
-    start_id = 0
-    while(counter <= 128):
-        img_chunk = img[start_id:(start_id + chunk_size)]
-        if len(img_chunk) < chunk_size:
-            break
-        else:
-            chunk_signature = hashlib.md5(img_chunk.tostring()).hexdigest()
-            img_signature += chunk_signature[int(len(chunk_signature)/2)]
-            counter += 1
-            start_id += chunk_size
+    vfunc = np.vectorize(applyMD5ToChunk)
+    img_chunks = np.split(img, range(chunk_size, len(img), chunk_size))
+    vf = vfunc(img_chunks, chunk_size)
+    img_signature = ''.join(vf)
     return (img_name, img_signature)
 
 def customHashFunc(str):
@@ -185,12 +196,14 @@ def lshTransform(kv, band_count = 16):
     img_name = kv[0]
     img_sig = kv[1]
     band_hash_list = []
-    row_size = int(128/band_count)
-    for i in range(0,128,row_size):
-        band_component = img_sig[i:(i+row_size)]
-        band_hash_list.append(customHashFunc(band_component))
-    # return something like ((file_name, band_id), bucket_number)
-    return [((band_hash_list[id],id),img_name) for id in range(len(band_hash_list))]
+    band_size = int(128/band_count)
+    img_sig_chunks = list(map(''.join, zip(*[iter(img_sig)]*band_size)))
+    vfunc = np.vectorize(customHashFunc)
+    band_hash_list = vfunc(img_sig_chunks)
+    # Key: (band_id, bucket_id)
+    zip_list_1 = list(zip(np.arange(band_size),band_hash_list))
+    img_name_list = [[img_name]]*band_size
+    return list(zip(zip_list_1,img_name_list))
 
 def filter4Sample(kv):
     """
@@ -269,75 +282,56 @@ def display2f(kv):
 def display3b(similarity_dict):
     for e in similarity_dict.keys():
         if e in ['3677454_2025195.zip-1','3677454_2025195.zip-18']:
-            pprint(similarity_dict[e])
+            print(e,' : ', similarity_dict[e])
 
 ###################################
-##  Set of function to compute SVD
+##  Set of functions to compute SVD
 ###################################
+"""
+Idea behind the SVD implementation done here:
+ - Here we are breaking the image into 500x 500 sub images, 
+    each of which contributes to a feature vector
+ - We have run a further intensity averaging, and down-sampling on each to create:
+    (img_component_i, 1x4900[np.array]), essentially a Key-Value pair 
+    where key is the image-component-name and value is the feature vector of shape 1x4900
+    
+ Broad Idea: 
+ -Each Spark RDD partition is of the form: 
+   [(img_comp-i, 1x4900 array),(img_comp-j, 1x4900 array),...,(img_comp-z, 1x4900 array)]
+ - We run SVD to transform it to corresponding low-dimensional form:  
+   [(img_comp-i, 1x10 array),(img_comp-j, 1x10 array),...,(img_comp-z, 1x10 array)]
+   
+ - We Use MapPartition function of pyspark to that end, to run our PartionedSVD
+ - For each partition, 
+        - partitionedSVD extracts the component-image-identifiers, and feature vectors
+        - np.stack is used to create a nx4900 dimensional matrix for the partition -- in order.
+        - We run normal SVD on this matrix, and reduce the dimension to nx10
+        - We recompose the Key Value pairs to get our target RDD partitions  
+"""
 
-def setupSVDdata(y, img_names):
-    """
-    Using the data set y = [(img_name-i,array[features]),(img_name-i,array[features]),...]
-    We create partitions such that each partition corresponds to an image, and we have:
-    [ (img_name1, array([[-1, -1,  1, ..., -1,  1, -1],...,[-1,  1,  1, ...,  1, -1, -1]]--> 2d array)),
-      (img_name2, array([[-1, -1,  1, ..., -1,  1, -1],...,[-1,  1,  1, ...,  1, -1, -1]]--> 2d array)),
-      ...,
-      (img_namen, array([[-1, -1,  1, ..., -1,  1, -1],...,[-1,  1,  1, ...,  1, -1, -1]]--> 2d array))
-    ]
-    i.e here each batch is a tuple and comprises of all 500x500 sub components of original image
-    stacked together row wise. The order of the rows is 0-24 as split up in the originally.
+def f(iterator):
+    for x in iterator:
+            print(x)
+    print("\n===\n")
 
-    This way we have a way to access low dimensional feature vecture by image name and component number.
-    '3677454_2025195.zip-18' feature vector will be available as
-    row 18 (starting from 0), of the batch which has key '3677454_2025195.zip'
-
-    Where each tuple has an image name and a matrix composed of it's feature vectors from
-    sub images as we broke them into 500x500 pieces in initial steps
-    :param y:
-    :param img_names:
-    :return:
-    """
-    batches = []
-    for img in img_names:
-        # For each image we get all component sub image feature vectors
-        batch_i = [i[0] for i in y if img in i[0]]
-        array_i = [j[1] for j in y if img in j[0]]
-        img_mat = np.stack(array_i)
-        batches.append((img, img_mat))
-    return batches
-
-def partitionedSVD(kv, low_dim_p=10):
-    """
-    Partitioned SVD function.
-    Recieves a (img_name, 2d array of feature vectors of component images)
-    Runs SVD on it, returns (img_name, 2d array of nx10)
-    :param kv:
-    :param low_dim_p:
-    :return:
-    """
-    img_name, mat = kv[0], kv[1]
-    # in order for SVD to perform PCA, the columns must be standardized:
-    # (also known as “zscore”: mean centers and divides by stdev)
-    mu, std = np.mean(mat, axis=0), np.std(mat, axis=0)
-    img_diffs_zs = (mat - mu) / std
+def partionedSVD(iterator, low_dim_p=10):
+    img_name_list = []
+    row_array_list = []
+    for x in iterator:
+        img_name_list.append(x[0])
+        row_array_list.append(x[1])
+    # Assemble all rows in the partition
+    # & create the Matrix
+    img_mat = np.stack(row_array_list)
+    # Normalizing - scaling & centering
+    mu, std = np.mean(img_mat, axis=0), np.std(img_mat, axis=0)
+    img_diffs_zs = (img_mat - mu) / std
     # run singular value decomposition on this .
     U, s, Vh = linalg.svd(img_diffs_zs, full_matrices=1)
     img_diffs_zs_lowdim = U[:, 0:low_dim_p]
-    return (img_name, img_diffs_zs_lowdim)
-
-def reattachSubImageNames(kv):
-    """
-    From (img_name, nd-array) tuples
-    we get back [(img_name-0, row_0),(img_name-1, row_1),(img_name-2, row_2)...]
-    :param kv tuple:
-    :return: list of tupples
-    :rtype list[]
-    """
-    img, mat = kv[0], kv[1]
-    component_list = []
-    for i in range(len(mat)):
-        component_list.append((img + '-' + str(i), mat[i]))
-    return component_list
+    ## Getting ready for throw back
+    transformed_partition = [(img_name_list[i],img_diffs_zs_lowdim[i]) for i in range(len(img_name_list))]
+    return transformed_partition
 
 def similarity_map(svd_collect):
     """
@@ -353,52 +347,140 @@ def similarity_map(svd_collect):
         sim_map[x[0]] = x[1]
     return dict(sim_map)
 
+############
+##
+## PY Spark Code Section
+##
+############
 # Timing tracker
 start_time = time.time()
+## Read Files
 rdd = sc.binaryFiles(_LOCAL_FILES_REGEX) #
 #rdd = sc.binaryFiles('hdfs:/data/large_sample')
 #rdd = sc.binaryFiles('hdfs:/data/small_sample')
-img_names = rdd.map(lambda x: x[0].split('/')[-1]).collect()
+
+## Obtain RDD as:[ (filename, tiffMatrix)...]
 rdd2 = rdd.map(lambda kv: getTiffAsMatrix(kv))
-rdd3 = rdd2.map(lambda kv: splitTiffArray(kv)).flatMap(lambda xs : [x for x in xs])
-##### Print Section for 1.e #####
-print1e = rdd3.filter(lambda x:display1e(x)).collect()
-for e in print1e:
-    print('(',e[0],',',e[1][0][0],')')
-#################################
+## Split each matrix to 500x500x4 images RDD: [ (img-0, 500x500x4),...(img-n, 500x500x4)]
+rdd3 = rdd2.flatMap(lambda kv: tiffmatrixSplit(kv))
+## Collect operation for 1.E
+data_for_print1e = rdd3.filter(lambda x:display1e(x)).collect()
+## Smooth out pixels to get RDD: [(img-0,500x500), (img-1,500x500) ...]
 rdd4 = rdd3.map(lambda kv: tilePixelIntensityConverter(kv))
+## Call Persist on RDD at this stage
 rdd4.persist()
+## Call down-scale of resolution on each sub image, default factor=10
+## Gives RDD[ (img-0,50x50),(img-1,50x50)...]
 rdd5 = rdd4.map(lambda kv: downScaleResolution(kv))
+## Computer Feature Vector for each image
 rdd6 = rdd5.map(lambda kv: getRowColDiffFeatureVec(kv))
-## Print section for 2.f ########
-print2f = rdd6.filter(lambda x: display2f(x)).collect()
-print(print2f)
-#################################
+
+########################
+##
+##  Locality Sensitive Hashing section
+##
+########################
+## Get Image signatures: [(img-0, 128char-0),(img-1, 128bsig-2)...]
 rdd7 = rdd6.map(lambda kv: getImageSignature(kv))
-rdd8 = rdd7.map(lambda kv: lshTransform(kv,16)).flatMap(lambda xs : [x for x in xs])
-rdd9 = rdd8.groupByKey().mapValues(list)
+## RUN LSH grouping
+rdd8 = rdd7.flatMap(lambda kv: lshTransform(kv,32))
+## reduce by (band, bucket) key
+rdd9 = rdd8.reduceByKey(lambda x,y: x+y)
+## Remove tuples which do not have target tuples.
 rdd10 = rdd9.filter(lambda kv: filter4Sample(kv))
+## Extract candidates for relevant images
 rdd11 = rdd10.map(lambda kv: candidateExtraction(kv)).filter(lambda kv: True if len(kv[1])>0 else False)
+print("Main Collect Step initiated for part 3...")
 candidate_set = rdd11.collect()
 end_time = time.time()
-print("Time elapsed: ", (end_time - start_time)/60,' mins \n')
+print("Time elapsed (upto LSH Completion): ", (end_time - start_time)/60,' mins \n')
 ## Collect from LSH similarity candidates
 similarity_dict = aggregateSimilarityDict(candidate_set)
-### Print 3b
-display3b(similarity_dict)
-###
-### Fire SVD similarity computation
-feature_vecs = rdd6.collect()
-dat = setupSVDdata(feature_vecs,img_names)
-data = sc.parallelize(dat)
-data_svd = data.map(lambda kv: partitionedSVD(kv))
-data_inv_map = data_svd.map(lambda kv: reattachSubImageNames(kv)).flatMap(lambda xs : [x for x in xs])
-d_collected = data_inv_map.collect()
+
+#########################
+##
+##  SVD Computation Section
+##  (Idea and function description in Function section)
+#########################
+rx = rdd6.mapPartitions(partionedSVD)
+print("SVD completion collect initiated")
+d_collected = rx.collect()
 similarity_map_dict = similarity_map(d_collected)
-## Distance
+
+######################
+##
+##   Output Section
+##
+#######################
+### 1E
+print(" ----------------OUTPUT 1.E --------------\n\n")
+for e in data_for_print1e:
+    print('(',e[0],',',e[1][0][0],')')
+
+print("\n\n")
+### 2F
+print(" ----------------OUTPUT 2.F --------------\n\n")
+print("('3677454_2025195.zip-1'",rdd6.lookup('3677454_2025195.zip-1')[0],")")
+print("('3677454_2025195.zip-18'",rdd6.lookup('3677454_2025195.zip-18')[0],")")
+print("\n\n")
+### 3B
+print(" ----------------OUTPUT 3.B --------------\n\n")
+display3b(similarity_dict)
+print("\n\n")
+### 3C
+print(" ----------------OUTPUT 3.C --------------\n\n")
+dist_tab = []
 for image in ['3677454_2025195.zip-1', '3677454_2025195.zip-18']:
     similar_images = similarity_dict[image]
     for c in similar_images:
-        print('dist(',image,',',c,'):= ', round(np.linalg.norm(similarity_map_dict[image] - similarity_map_dict[c]),3) )
+        dist = round(np.linalg.norm(similarity_map_dict[image] - similarity_map_dict[c]), 3)
+        dist_tab.append(((image,c), dist))
+
+pprint(sorted(dist_tab,key=lambda x: x[1]))
 end_time = time.time()
-print("Time elapsed: ", (end_time - start_time)/60,' mins \n')
+print("Total Time elapsed before Extra Creds: ", (end_time - start_time)/60,' mins \n')
+
+# ###########################
+# ##
+# ##  Extra Credit Things
+# ##
+# ############################
+# rdd5 = rdd4.map(lambda kv: downScaleResolution(kv,5)) # Shape is 100x100
+# rdd6 = rdd5.map(lambda kv: getRowColDiffFeatureVec(kv)) # Feature Vector: 19800
+# # Bonus LSH Section
+# rdd7 = rdd6.map(lambda kv: getImageSignature(kv,154)) #chunk_size=154
+# rdd8 = rdd7.flatMap(lambda kv: lshTransform(kv,4))
+# rdd9 = rdd8.reduceByKey(lambda x,y: x+y)
+# rdd10 = rdd9.filter(lambda kv: filter4Sample(kv))
+# rdd11 = rdd10.map(lambda kv: candidateExtraction(kv)).filter(lambda kv: True if len(kv[1])>0 else False)
+# print("Bonus Collect Step initiated for part 3...")
+# candidate_set = rdd11.collect()
+# end_time = time.time()
+# print("Time elapsed (upto Bonus LSH Completion): ", (end_time - start_time)/60,' mins \n')
+# ## Collect from LSH similarity candidates
+# similarity_dict = aggregateSimilarityDict(candidate_set)
+#
+# ## SVD Computation Section
+# rx = rdd6.mapPartitions(partionedSVD)
+# print("SVD completion collect initiated")
+# d_collected = rx.collect()
+# similarity_map_dict = similarity_map(d_collected)
+#
+# ##   LSH & SVD Bonus Output Section
+#
+# print(" ----------------Bonus OUTPUT 3.B --------------\n\n")
+# display3b(similarity_dict)
+# print("\n\n")
+# ### 3C
+# print(" ----------------Bonus OUTPUT 3.C --------------\n\n")
+# dist_tab = []
+# for image in ['3677454_2025195.zip-1', '3677454_2025195.zip-18']:
+#     similar_images = similarity_dict[image]
+#     for c in similar_images:
+#         dist = round(np.linalg.norm(similarity_map_dict[image] - similarity_map_dict[c]), 3)
+#         dist_tab.append(((image,c), dist))
+#
+# print(sorted(dist_tab,key=lambda x: x[1]))
+# end_time = time.time()
+# print("Total Time elapsed since start: ", (end_time - start_time)/60,' mins \n')
+
